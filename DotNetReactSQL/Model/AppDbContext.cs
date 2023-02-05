1cb864631b492bfc87e61d709882bed15f0630c8
Ryan Gomes <ryangomes97@gmail.com>
2023-02-05 21:37:31 +0000

    Add project files.

---
+++ DotNetReactSQL/Model/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotNetReactSQL.Model
+{
+    public class AppDbContext : DbContext
+    {
+        private IConfiguration _configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+        public DbSet<PhoneBook> PhoneBook { get; set; } 
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string? connectionString = _configuration.GetConnectionString("WebApiDatabase");
+            if (connectionString != null)
+            {
+                optionsBuilder.UseMySQL(connectionString);
+            }
+            else
+            {
+                throw new Exception("Connection does not exist");
+            }
+            
+        }
+    }
+}
---
+++ DotNetReactSQL/Model/PhoneBook.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetReactSQL.Model
+{
+    public class PhoneBook
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+    }
+}
